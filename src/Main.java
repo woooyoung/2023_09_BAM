7a6bfb171a84fbaf567ca7052b69791802b0e238
woooyoung <axdsw121@gmail.com>
2023-09-14 14:05:25 +0900

    게시물 작성시 날짜 및 시간 저장

--- src/Main.java
+++ src/Main.java
@@ -39,12 +39,13 @@ public class Main {
 
 			} else if (command.equals("article write")) {
 				int id = lastArticleId + 1;
+				String regDate = Util.getNow();
 				System.out.printf("제목 : ");
 				String title = sc.nextLine();
 				System.out.printf("내용 : ");
 				String body = sc.nextLine();
 
-				Article article = new Article(id, title, body);
+				Article article = new Article(id, regDate, title, body);
 				articles.add(article);
 
 				System.out.printf("%d번글이 생성되었습니다.\n", id);
@@ -62,7 +63,7 @@ public class Main {
 					if (article.id == id) {
 						found = true;
 						System.out.println("번호 : " + article.id);
-						System.out.println("날짜 : " + "2023-12-12 12:12:12");
+						System.out.println("날짜 : " + article.regDate);
 						System.out.println("제목 : " + article.title);
 						System.out.println("내용 : " + article.body);
 						break;
@@ -87,11 +88,19 @@ public class Main {
 
 class Article {
 	int id;
+	String regDate;
 	String title;
 	String body;
 
 	Article(int id, String title, String body) {
 		this.id = id;
+		this.title = title;
+		this.body = body;
+	}
+
+	Article(int id, String regDate, String title, String body) {
+		this.id = id;
+		this.regDate = regDate;
 		this.title = title;
 		this.body = body;
 	}
---
+++ src/Util.java
@@ -0,0 +1,16 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Util {
+	// 현재 날짜 및 시간 리턴 String
+	public static String getNow() {
+
+		// 현재 날짜/시간
+		LocalDateTime now = LocalDateTime.now();
+
+		// 포맷팅
+		String formatedNow = now.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+
+		return formatedNow;
+	}
+}
