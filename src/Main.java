a24a6326642ba2de8a05b2f71df473e9a81b0aed
woooyoung <axdsw121@gmail.com>
2023-09-19 14:13:21 +0900

    회원가입 기능 추가, 비밀번호 확인, 필수입력 정보

--- src/Main.java
+++ src/Main.java
@@ -35,6 +35,9 @@ public class Main {
 				int id = lastMemberId + 1;
 				String regDate = Util.getNow();
 				String loginId = null;
+				String loginPw = null;
+				String loginPwConfirm = null;
+				String name = null;
 
 				while (true) {
 					System.out.printf("로그인 아이디 : ");
@@ -51,12 +54,41 @@ public class Main {
 					break;
 				}
 
-				System.out.printf("로그인 비밀번호 : ");
-				String loginPw = sc.nextLine();
-				System.out.printf("로그인 비밀번호 확인 : ");
-				String loginPwConfirm = sc.nextLine();
-				System.out.printf("이름 : ");
-				String name = sc.nextLine();
+				while (true) {
+					System.out.printf("로그인 비밀번호 : ");
+					loginPw = sc.nextLine();
+					if (loginPw.length() == 0) {
+						System.out.println("비밀번호 입력해라");
+						continue;
+					}
+					while (true) {
+						System.out.printf("로그인 비밀번호 확인 : ");
+						loginPwConfirm = sc.nextLine();
+
+						if (loginPwConfirm.length() == 0) {
+							System.out.println("비밀번호 확인 입력해라");
+							continue;
+						}
+						break;
+					}
+
+					if (loginPw.equals(loginPwConfirm) == false) {
+						System.out.println("비밀번호 확인해");
+						continue;
+					}
+					break;
+				}
+
+				while (true) {
+					System.out.printf("이름 : ");
+					name = sc.nextLine();
+
+					if (name.length() == 0) {
+						System.out.println("이름 입력해라");
+						continue;
+					}
+					break;
+				}
 
 				Member member = new Member(id, regDate, regDate, loginId, loginPw, name);
 				members.add(member);
